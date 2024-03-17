15fd266134ad3d59a14fa5a3d3c6d227913f0f33
Yasir Z <mazakhus2@gmail.com>
2024-03-17 11:51:12 +0530

    Added Collaboration aspect but there are some issues.

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
 
 @app.route('/')
-def HelloWorld():
-    return 'Hello world'
+def Index():
+    return render_template('index.html')
+
+@socketio.on('code_change')
+def handle_code_change(data):
+    code = data['code']
+    emit('code_change', {'code': code}, broadcast=True)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,use_reloder=True)
+    socketio.run(app)
