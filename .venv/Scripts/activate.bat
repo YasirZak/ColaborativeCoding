e26ee17df37367299f546faf202c25c779e2ac18
Yasir Z <mazakhus2@gmail.com>
2024-04-26 12:27:39 +0530

    Revert "Merge branch 'main' into twisterpact2"
    This reverts commit b1e8b0960d2e8868c905d0c2dc0a64c691b1b523, reversing
    changes made to d986cb088b43ce6bb87af29b2cd612bd41be393f.

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\venka\OneDrive\Documents\GitHub\ColaborativeCoding\.venv
+set VIRTUAL_ENV=E:\ColaborativeCoding\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
